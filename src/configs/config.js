04f3090f1c7ef41f796641395bc175d81af3bdb7
Hưng Nguyễn <hungnguyen101196.com>
2024-06-27 04:21:57 +0700

    add services

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,3 +1,1 @@
-export const API_URL = process.env.API_URL
-export const SUPABASE_URL = process.env.SUPABASE_URL
-export const SUPABASE_KEY = process.env.SUPABASE_KEY
+export const API_URL = 'https://pink-panthers-assignment-be.onrender.com'
