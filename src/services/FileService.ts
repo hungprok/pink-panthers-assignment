7bf63e1a47ca31cb45cb0a1873b0f07608c380fd
Hưng Nguyễn <hungnguyen101196.com>
2024-06-27 03:51:22 +0700

    add services

--- src/services/FileService.ts
+++ src/services/FileService.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
+import { API_URL } from '../config.js'
+
 const requestOptions = {
     headers: { 'Content-Type': 'formdata/multipart' }
 }
 export class FileService {
     async upload(payload: FormData) {
-        return axios.post(`http://localhost:3000/upload`, payload, requestOptions)
+        return axios.post(`${API_URL}/upload`, payload, requestOptions)
     }
 }
--- src/services/ItemService.ts
+++ src/services/ItemService.ts
@@ -1,27 +1,28 @@
 import axios from "axios";
 import type { Item } from "@/interfaces/item"
+import { API_URL } from '../config.js'
 const requestOptions = {
     headers: { 'Content-Type': 'application/json' }
 }
 export class ItemService {
     addItem(payload: Item) {
 
-        return axios.post(`http://localhost:3000/items`, payload, requestOptions)
+        return axios.post(`${API_URL}/items`, payload, requestOptions)
     }
 
     updateItem(payload: Item) {
-        return axios.patch(`http://localhost:3000/items/${payload.id}`, payload, requestOptions)
+        return axios.patch(`${API_URL}/items/${payload.id}`, payload, requestOptions)
     }
 
     deleteItem(id: number) {
-        return axios.delete(`http://localhost:3000/items/${id}`)
+        return axios.delete(`${API_URL}/items/${id}`)
     }
 
     getAllItems(): Promise<Item> {
-        return axios.get(`http://localhost:3000/items`, requestOptions)
+        return axios.get(`${API_URL}/items`, requestOptions)
     }
 
     getItemById(id: number): Promise<Item> {
-        return axios.get(`http://localhost:3000/items/${id}`, requestOptions)
+        return axios.get(`${API_URL}/items/${id}`, requestOptions)
     }
 }
